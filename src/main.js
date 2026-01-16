c9e02391ae639346f0d835365d4f88c6c1700f55
BXAMRA <44099426+BXAMRA@users.noreply.github.com>
2026-01-16 22:04:50 +0530

    Set up Vue Router with Home and About routes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from "vue";
+import { createRouter, createWebHashHistory } from "vue-router";
 import "./style.css";
 import App from "./App.vue";
-import router from "./router";
+
+// Router Pages
+import Home from "@views/Home.vue";
+import About from "@views/About.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+];
+
+const router = createRouter({ history: createWebHashHistory(), routes });
 
 createApp(App).use(router).mount("#app");
